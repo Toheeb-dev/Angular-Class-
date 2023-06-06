cca0d58ce26778cc28db73d4ad656b4bdad35984
Toheeb-dev <toheeb.dev1@gmail.com>
2023-06-06 07:19:02 -0700

    added local reference topic

--- src/app/veiw-child/veiw-child.component.ts
+++ src/app/veiw-child/veiw-child.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-veiw-child',
   templateUrl: './veiw-child.component.html',
   styleUrls: ['./veiw-child.component.css']
 })
-export class VeiwChildComponent {
+export class VeiwChildComponent implements OnInit {
+
+  constructor(){  }
+
+  ngOnInit(): void {
+    
+  }
+
+  title= 'viewChild';
+  @ViewChild('myDOB') dateOfbirth: ElementRef ;
+  @ViewChild('myAge') age: ElementRef ;
+
+  calculateAge(){
+    let birthYear = new Date()
+  }
+
 
 }
